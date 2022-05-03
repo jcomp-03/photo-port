184aa4990f405e3aca2d849e61fdeea2efc60396
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-05-03 12:23:36 -0400

    Created React component 'Nav' & 'About'. Saving work.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import coverImage from "../../assets/cover/cover-image.jpg";
 
 const About = () => {
     return (
-        <section>
-            <h1>Who am I?</h1>
+        <section className="my-5">
+            <h1>Who am I? A figment of your imagination.</h1>
+            <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
         </section>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { ReactDOM, render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const rootElement = document.getElementById('root');
+render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>, 
+  rootElement
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
