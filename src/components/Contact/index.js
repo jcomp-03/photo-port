4e70f46073964367352d3be77e3a168d39972d05
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-05-08 15:57:42 -0400

    Created controlled component ContactForm which handles input changes and submit click

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
   //JSX
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
 
+  function handleChange(e) {
+    // the ... is the spread operator. We use the spread operator ...formState
+    // so we can retain the other key-value pairs in this object.
+    // Without the spread operator, the formState object would be overwritten
+    // to only contain the name: value key pair
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+  
   return (
     <section>
       <h1>Contact me</h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            defaultValue={name}
+            onChange={handleChange}
+            name="name"
+          />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" />
+          <input
+            type="email"
+            defaultValue={email}
+            onChange={handleChange}
+            name="email"
+          />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea
+            defaultValue={message}
+            onChange={handleChange}
+            name="message"
+            rows="5"
+          />
         </div>
         <button type="submit">Submit</button>
       </form>
